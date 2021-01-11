be8a53cb756af88699abcb8c3fa81ea72164c8c8
yuna <mapleyuna3262@gmail.com>
2021-01-11 21:52:55 +0900

    Home api

--- src/components/CarouselOfHome.js
+++ src/components/CarouselOfHome.js
@@ -1,34 +1,42 @@
 import React from 'react';
 import ItemsCarousel from 'react-items-carousel';
-import range from 'lodash/range';
 import TRAINERIMG from './img/trainer_img.png';
 import './CarouselOfHome.css';
+import axios from 'axios';
+const dataURL = 'http://chatt.ngrok.io/api/course/populars/';
 
 export default class Test extends React.Component {
+    state = {
+        datas: [],
+    };
+    getTopClasses = async() => {
+        const { data } = await axios.get(dataURL);
+        this.setState( {datas: data });
+    }
     componentWillMount() {
+        this.getTopClasses();
         this.setState({
         children: [],
         activeItemIndex: 0,
-    });
-    
-    setTimeout(() => {
-        this.setState({
-            children: range(3).map(i => 
-            <div class="oneclass_container">
-                <div class="oneclass_keyword_container">
-                    <div class="oneclass_keyword" id="type">방문</div>
-                    <div class="oneclass_keyword" id="hashtag">#필라테스</div>
-                </div>
-                <div class="oneclass_description">
-                    바른자세 맑은정신을 위한 운동! 시작해보세요.
-                </div>
-                <div class="oneclass_trainer">
-                    <img class="oneclass_trainer_img" src={TRAINERIMG}/>
-                    <div class="oneclass_trainer_name">제니퍼 트레이너</div>
-                </div>
-            </div>),
-        })
-    }, 100);
+        });
+        setTimeout(() => {
+            this.setState({
+                children: this.state.datas.map((data) => 
+                <div class="oneclass_container">
+                    <div class="oneclass_keyword_container">
+                        <div class="oneclass_keyword" id="type">방문</div>
+                        <div class="oneclass_keyword" id="hashtag">#필라테스</div>
+                    </div>
+                    <div class="oneclass_description">
+                        {data.information}
+                    </div>
+                    <div class="oneclass_trainer">
+                        <img class="oneclass_trainer_img" src={TRAINERIMG}/>
+                        <div class="oneclass_trainer_name">{data.teacher.name}</div>
+                    </div>
+                </div>),
+            })
+        }, 1000);
     }
     changeActiveItem = (activeItemIndex) => this.setState({ activeItemIndex });
   
